6fc80a34de7c13ae4f9389836248b62b71de2a8f
Vijay Shanker <vshank3@ext.uber.com>
2019-02-19 15:50:47 +0530

    basic property functioning and computed attributes

---
+++ 01property.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env/ python
+
+
+# order of fget, fset and fdel is important
+# pay attention to type of Person.name and method of this property object
+# properties are inheritable, see Employee class.
+
+
+class Person(object):
+	def __init__(self, name):
+		self._name = name
+
+	def fget(self):
+		"""
+		get me person's name
+		"""
+		print "Fetch name"
+		return self._name
+
+	def fset(self, value):
+		print "Set name"
+		self._name = value
+
+	def fdel(self):
+		print "Deleteing name attribute"
+		del self._name
+
+	name = property(fget, fset, fdel, "Name property doc")
+
+
+class Employee(Person):
+	pass
+
+
+if __name__ == "__main__":
+	person = Person("Vijay")
+	print "Docstring for person's name attribute: {}".format(Person.name.__doc__)
+	print "Printing person's name : {}".format(person.name)
+	person.name = "Shanker"
+	del person.name
+	print "-------------------------------"
+	emp = Employee("Hari")
+	print emp.name
+
+	print "-------------------------------"
+	print type(Person.name)
+	print "-------------------------------"
+	print dir(Person.name)
+	print "-------------------------------"	
---
+++ 02computed_attributes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+class PropSquare(object):
+	def __init__(self, value):
+		self.value = value
+
+	def squaredx(self):
+		return self.value**2
+
+	def setvalue(self, value):
+		self.value = value
+
+	def delvalue(self):
+		del self.value
+ 	x = property(squaredx, setvalue, delvalue, "computed attribute example")
+
+
+if __name__ == "__main__":
+	p = PropSquare(4)
+	print p.x
+	p.x = 6
+	print p.x
